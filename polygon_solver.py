d8a38475565ed5b6417eda5bc522480581262109
psyrj5 <psyrj5@exmail.nottingham.ac.uk>
2023-01-08 12:46:17 +0000

    Print by size

--- polygon_solver.py
+++ polygon_solver.py
@@ -17,31 +17,40 @@ def main():
     dictionary = dict_list[DICTIONARY]
 
     if argc <= 3:
+        print("too few args")
         return 1
     # if argc > 4 and "true" == sys.argv[4]:
     #     dictionary = dict_list[0]
 
-    available_letters = sys.argv[1]
-    required_letter = sys.argv[2]
+    available_letters = sys.argv[1].upper()
+    required_letter = sys.argv[2].upper()
     min_size = sys.argv[3]
     max_size = len(available_letters)
 
+    debug = False
+
+    if argc >= 5:
+        debug = True
+
+    if debug:
+        print(available_letters, required_letter, min_size)
+
     if len(required_letter) != 1:
+        print("required must be one")
         return 1
     try:
-        min_size = int(min)
+        min_size = int(min_size)
     except:
+        print("cannot make min size an int")
         return 1
     
     # start program
     
     with open(dictionary) as f:
         all_words = json.load(f)
-
-    perms = []
-    for i in range(min_size, max_size+1):
-        perms = perms + list(itertools.permutations(available_letters, i))
-
+        if debug:
+            all_words["hello"]
+        
     def validate(word):
         if not required_letter in word:
             return False
@@ -49,10 +58,18 @@ def main():
             return False
         return True
 
-    valid = sorted(list(set(filter(validate, map(lambda x : "".join(x), perms)))), key=len)
+    valid = []
+    for i in range(min_size, max_size+1):
+        i_perms = list(itertools.permutations(available_letters, i))
+        i_perms = list(set(map(lambda x : x.lower(), list(filter(validate, map(lambda x : "".join(x), i_perms))))))
+        valid.extend(i_perms)
 
-    print(len(valid))
-    print(valid)
+        print(i,":",i_perms)
+
+    # valid = sorted(list(set(filter(validate, map(lambda x : "".join(x), perms)))), key=len)
+
+    print(len(valid), "Words Found")
+    # print(list(map(lambda x : x.lower(), valid)))
 
 main()
 # LETTERS = "grievpeil"
